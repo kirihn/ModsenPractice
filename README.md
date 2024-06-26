d49aeb00144b06b276d81bb9af9ab71a87625a63
Zukay Kiryl <kzykov03@mail.ru>
2024-06-26 13:57:23 +0300

    fix: change readme

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # ModsenPractice
+# deploy https://modsenpractice-1.onrender.com/
+
+редакс пока находится на стадии изучения, прошу проигнорировать этот блок
 
 background: rgba(79, 0, 182, 0.3);
 GoogleBooksApiKey: AIzaSyBRgz0ELM__gIfm5fbGPcexaOO5P0Kf-wM
--- book-search-wp-ts/src/app.tsx
+++ book-search-wp-ts/src/app.tsx
@@ -10,7 +10,7 @@ export default function App() {
         <>
             <Header></Header>
             <main>
-                <Counter></Counter>
+                {/* <Counter></Counter> */}
                 <BookList></BookList>
                 <BookPage></BookPage>
             </main>
